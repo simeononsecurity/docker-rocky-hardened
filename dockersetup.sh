e7771e7cd2ad0f05b593dd452b82bdf3cd82c595
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2022-08-28 22:41:17 -0500

    Init

---
+++ dockersetup.sh
@@ -0,0 +1,3 @@
+ansible-galaxy install konstruktoid.hardening
+ansible-playbook -i localhost /docker-rocky-hardened/playbook.yml
+ansible-playbook -e ignore_errors=yes -v -b -i /dev/null /U_RHEL_8_V1R6_STIG_Ansible/rhel8STIG-ansible/site.yml
